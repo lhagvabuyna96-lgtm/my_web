ca79a9de3516f0dfdeb46c97a56907ea938f6cb5
Haruto <lhagvabuyna96@gmail.com>
2026-05-05 02:30:11 +0800

    Connect to Render backend

--- auth-config.js
+++ auth-config.js
@@ -1,5 +1,5 @@
 (function (global) {
   global.HARUTO_AUTH = {
-    API_URL: "http://localhost:3000/api",
+    API_URL: "https://backend-puz8.onrender.com/api",
   };
 })(typeof window !== "undefined" ? window : this);
